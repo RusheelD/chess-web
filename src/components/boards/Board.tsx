119e27d4f513b927cb61ee6f4b287ab68e68829b
Rusheel <rusheel@live.com>
2022-05-03 01:45:23 -0700

    implimented clicking on tiles

--- src/components/boards/Board.tsx
+++ src/components/boards/Board.tsx
@@ -10,11 +10,17 @@ export interface BoardProps {
 }
 
 export function Board(props: BoardProps) {
+    var pieceMap = new Map<string, PieceProps>(Object.entries(pieces));
+    /*return(
+        <div style={{display: 'inline-block'}}>
+            {[...Array(8)].map((x, i) => 
+                [...Array(8)].map((y, j) => 
+                    <Tile rank={(8-i).toString()} file={String.fromCharCode(j + 97)} children={<Piece color={pieceMap.get(String.fromCharCode(j + 97)+(8-i).toString())!.color} name={pieceMap.get(String.fromCharCode(j + 97)+(8-i).toString())!.name}/>}/>
+                )
+            )}
+        </div>
+    );*/
     var rows = [];
-    var pieceMap = new Map<string, PieceProps>();
-    for (var k in Object.keys(pieces)) {
-        pieceMap.set(Object.keys(pieces)[k], Object.values(pieces)[k]);
-    }
     for (var j = 8; j > 0; j -= 1) {
         var tiles = [];
         for (var i = 97; i < 105; i += 1) {
@@ -29,7 +35,6 @@ export function Board(props: BoardProps) {
         }
         rows.push(<div style={{display:'inline-block'}} className={'row'} children={tiles}></div>);
     }
-    // var span = React.createElement('span', {className: 'board'})
     var div = React.createElement('div', {display: 'inline-block'}, [props.children, rows])
     return(div);
 }
--- src/components/tiles/Tile.tsx
+++ src/components/tiles/Tile.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Tile.css';
 
 export interface TileProps {
@@ -6,14 +7,15 @@ export interface TileProps {
     children: any;
 }
 
-export function Tile(props: TileProps) { 
+export function Tile(props: TileProps) {
+    const [clickStatus, setClickStatus] = useState(false);
     var scale = (parseInt(props.rank)%2 + (props.file.charCodeAt(0) - 96)%2)%2;
     var tileStyle = scale ? 'whiteTile' : 'blackTile';
     return(
-    <div style={{display: 'inline-block'}}>
-        {<span className={'tile ' + props.file + ' _' + props.rank + ' ' + tileStyle}>
+    <div style={{display: 'inline-block'}} className={tileStyle} onClick={() => setClickStatus(!clickStatus)}>
+        {<span className={'tile ' + props.file + ' _' + props.rank + ' ' + (clickStatus ? "clicked" : "")}>
             {props.children}
         </span>}
     </div>
-);
+    );
 }
