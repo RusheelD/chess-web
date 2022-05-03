64aa716782ac6aa5f08acd8ea4c2c6aedc7c120b
Rusheel <rusheel@live.com>
2022-05-03 00:13:09 -0700

    added pieces starting positions

--- src/components/boards/Board.tsx
+++ src/components/boards/Board.tsx
@@ -1,20 +1,31 @@
+import { isValidInputTimeValue } from '@testing-library/user-event/dist/utils';
 import React from 'react';
-import { Piece } from '../pieces/Piece';
+import { Piece, PieceProps } from '../pieces/Piece';
 import { Tile } from '../tiles/Tile'
 import './Board.css';
+import {default as pieces} from "./initial.json";
 
 export interface BoardProps {
     children:any;
 }
 
-export function Board(props: BoardProps) { 
+export function Board(props: BoardProps) {
     var rows = [];
+    var pieceMap = new Map<string, PieceProps>();
+    for (var k in Object.keys(pieces)) {
+        pieceMap.set(Object.keys(pieces)[k], Object.values(pieces)[k]);
+    }
     for (var j = 8; j > 0; j -= 1) {
         var tiles = [];
         for (var i = 97; i < 105; i += 1) {
             var file = String.fromCharCode(i);
             var rank = j.toString();
-            tiles.push(<Tile rank={rank} file={file} children={null}/>);
+            var tile = file+rank;
+            var child = null;
+            if(pieceMap.has(tile)) {
+                child = <Piece color={pieceMap.get(tile)!.color} name={pieceMap.get(tile)!.name}/>
+            }
+            tiles.push(<Tile rank={rank} file={file} children={child}/>);
         }
         rows.push(<div style={{display:'inline-block'}} className={'row'} children={tiles}></div>);
     }
