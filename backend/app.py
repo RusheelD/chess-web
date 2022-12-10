fdf0ed20c55138fa13c58d212e128aaca5c0d9ab
Rusheel Dasari <rusheel@gmail.com>
2022-12-10 14:13:02 -0800

    sockets working classic

--- backend/app.py
+++ backend/app.py
@@ -1,55 +1,15 @@
-import json
-
-import flask
-import flask_socketio
+'''import flask
+import flask_socketio'''
 from AIControl import AIControl
 from GameControl import GameControl
 from MultiGameControl import MultiGameControl
 
-"""
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 import socketio
 import uvicorn
-
-
-fastapp = FastAPI()
-fastapp.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-socket_manager = socketio.AsyncServer(
-    async_mode='asgi', cors_allowed_origins='*')
-app = socketio.ASGIApp(socket_manager, fastapp)
-
-
-class AppManager():
-    def __init__(self, app, socket_manager):
-        self.app = app
-        self.updated_data = None
-        self.games = {}
-        self.socket_manager = socket_manager
-        self.users = AllPlayersData()
-
-app_manager = AppManager(app, socket_manager)
-"""
-
-app = flask.Flask(__name__)
-
-
-# AppManager class to contain and handle the details of a specific app and login manager
-# to maintain the state and manage the logic
-class AppManager():
-    def __init__(self, app):
-        self.app = app
-        self.updated_data = None
-        self.games = {}
-        self.socketIO = flask_socketio.SocketIO(
-            self.app, cors_allowed_origins='*')
-        self.users = AllPlayersData()
 
 
 class GameData():
@@ -100,9 +60,284 @@ class AllPlayersData():
         return self.players[item]
 
 
+fastapp = FastAPI()
+fastapp.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+socket_manager = socketio.AsyncServer(
+    async_mode='asgi', cors_allowed_origins='*')
+app = socketio.ASGIApp(socket_manager, fastapp)
+
+
+class AppManager():
+    def __init__(self, app, socket_manager):
+        self.app = app
+        self.updated_data = None
+        self.games = {}
+        self.socket_manager = socket_manager
+        self.users = AllPlayersData()
+
+
+app_manager = AppManager(app, socket_manager)
+
+
+@app_manager.app.other_asgi_app.get('/')
+def root_get():
+    return RedirectResponse(url='/index')
+
+
+@app_manager.app.other_asgi_app.post('/')
+def root_post():
+    return RedirectResponse(url='/index')
+
+
+@app_manager.app.other_asgi_app.post('/index')
+def index_get():
+    return {"status": "OK"}
+
+
+@app_manager.app.other_asgi_app.post('/index')
+def index_post():
+    return {"status": "OK"}
+
+
+@app_manager.app.other_asgi_app.get("/add")
+async def add_get(request: Request):
+    data = dict(await request.json())
+    usercode = data.get("code")
+    username = data.get("name")
+    userid = data.get('id')
+    app_manager.users.add(PlayerData(usercode, username, userid))
+    return app_manager.users.to_dict(usercode)
+
+
+@app_manager.app.other_asgi_app.post("/add")
+async def add_post(request: Request):
+    data = dict(await request.json())
+    usercode = data.get("code")
+    username = data.get("name")
+    userid = data.get('id')
+    app_manager.users.add(PlayerData(usercode, username, userid))
+    return app_manager.users.to_dict(usercode)
+
+
+@app_manager.app.other_asgi_app.get("/make")
+async def make_get(request: Request):
+    data = dict(await request.json())
+    gamecode = data.get("code")
+    player = app_manager.users[data.get('usercode')]["player"]
+    if gamecode not in app_manager.games:
+        mode = data.get('mode')
+        gameType = data.get('type')
+        if (mode == 'synchronic'):
+            app_manager.games[gamecode] = GameData(
+                MultiGameControl(), 0, AllPlayersData().add(player))
+        elif (mode == 'classic'):
+            if (gameType == 'ai'):
+                app_manager.games[gamecode] = GameData(
+                    AIControl(), 0, AllPlayersData().add(player))
+            elif (gameType == "network"):
+                app_manager.games[gamecode] = GameData(
+                    GameControl(), 0, AllPlayersData().add(player))
+            else:
+                app_manager.games[gamecode] = GameData(
+                    GameControl(), 0, AllPlayersData().add(player))
+        return board_data(code=gamecode)
+    app_manager.games[gamecode].logins += 1
+    if (player.code not in app_manager.games[gamecode].users.players):
+        app_manager.games[gamecode].users.add(player)
+    await app_manager.socket_manager.emit("select", board_data(data.get('code')),
+                                          namespace='/', broadcast=True)
+    return board_data(code=gamecode, usercode=data.get("usercode"))
+
+
+@app_manager.app.other_asgi_app.post("/make")
+async def make(request: Request):
+    data = dict(await request.json())
+    gamecode = data.get("code")
+    player = app_manager.users[data.get('usercode')]["player"]
+    if gamecode not in app_manager.games:
+        mode = data.get('mode')
+        gameType = data.get('type')
+        if (mode == 'synchronic'):
+            app_manager.games[gamecode] = GameData(
+                MultiGameControl(), 0, AllPlayersData().add(player))
+        elif (mode == 'classic'):
+            if (gameType == 'ai'):
+                app_manager.games[gamecode] = GameData(
+                    AIControl(), 0, AllPlayersData().add(player))
+            elif (gameType == "network"):
+                app_manager.games[gamecode] = GameData(
+                    GameControl(), 0, AllPlayersData().add(player))
+            else:
+                app_manager.games[gamecode] = GameData(
+                    GameControl(), 0, AllPlayersData().add(player))
+        return board_data(code=gamecode)
+    app_manager.games[gamecode].logins += 1
+    if (player.code not in app_manager.games[gamecode].users.players):
+        app_manager.games[gamecode].users.add(player)
+    await app_manager.socket_manager.emit("select", board_data(data.get('code')),
+                                          namespace='/', broadcast=True)
+    return board_data(code=gamecode, usercode=data.get("usercode"))
+
+
+def board_data(code, usercode=None):
+    game = app_manager.games[code].game
+    return {
+        "board": game.main_board.to_fen(),
+        "possible": game.main_board.format_valid_moves(game.selected_piece),
+        "dead_white": game.main_board.get_dead()[0],
+        "dead_black": game.main_board.get_dead()[1],
+        "move_counts": game.main_board.move_count_fen(),
+        "recent": game.send_recent(),
+        "current_player": game.current_player(),
+        "current_piece": game.current_piece(),
+        "check": game.send_check(),
+        "checkmate": game.send_checkmate(),
+        "stalemate": game.send_stalemate(),
+        "logged": app_manager.games[code].logins,
+        "users": app_manager.games[code].users.to_dict(usercode)
+    }
+
+
+@app_manager.app.other_asgi_app.get("/board")
+async def board_get(request: Request):
+    data = dict(await request.json())
+    code = data.get('code')
+    usercode = data.get('usercode')
+    return board_data(code=code, usercode=usercode)
+
+
+@app_manager.app.other_asgi_app.post("/board")
+async def board_post(request: Request):
+    data = dict(await request.json())
+    code = data.get('code')
+    usercode = data.get('usercode')
+    return board_data(code=code, usercode=usercode)
+
+
+@app_manager.app.other_asgi_app.get("/select")
+async def select_get(request: Request):
+    data = dict(await request.json())
+    game = app_manager.games[data.get('code')].game
+
+    if (game is None):
+        return
+    tile = data.get('tile')
+    choice = data.get('choice')
+    tup = (int(tile[1]) - 1, ord(tile[0])-97)
+    game.select_tile(tup[0], tup[1], choice=choice)
+
+    if (not (game.is_piece_selected)):
+        await app_manager.socket_manager.emit("select", board_data(data.get('code')),
+                                              namespace='/', broadcast=True)
+    return "selected"
+
+
+@app_manager.app.other_asgi_app.post("/select")
+async def select_post(request: Request):
+    data = dict(await request.json())
+    game = app_manager.games[data.get('code')].game
+
+    if (game is None):
+        return
+    tile = data.get('tile')
+    choice = data.get('choice')
+    tup = (int(tile[1]) - 1, ord(tile[0])-97)
+    game.select_tile(tup[0], tup[1], choice=choice)
+
+    if (not (game.is_piece_selected)):
+        await app_manager.socket_manager.emit("select", board_data(data.get('code')),
+                                              namespace='/', broadcast=True)
+    return "selected"
+
+
+"""
+@app_manager.socket_manager.on("select")
+def handle_select(*args):
+    code, tile, choice = data
+    game = app_manager.games[code].game
+    if (game is None):
+        return
+    tup = (int(tile[1]) - 1, ord(tile[0])-97)
+    game.select_tile(tup[0], tup[1], choice=choice)
+    if (not (game.is_piece_selected)):
+        await app_manager.socket_manager.emit("select", board_data(
+            code), namespace='/', broadcast=True)
+"""
+
+
+@app_manager.app.other_asgi_app.get("/reset")
+async def reset_get(request: Request):
+    data = dict(await request.json())
+    game = app_manager.games[data.get('code')].game
+    app_manager.games[data.get('code')] = GameData(
+        type(game)(), 0, app_manager.games[data.get('code')].users)
+    return board_data(code=data.get('code'))
+
+
+@app_manager.app.other_asgi_app.post("/reset")
+async def reset_post(request: Request):
+    data = dict(await request.json())
+    game = app_manager.games[data.get('code')].game
+    app_manager.games[data.get('code')] = GameData(
+        type(game)(), 0, app_manager.games[data.get('code')].users)
+    return board_data(code=data.get('code'))
+
+
+@app_manager.app.other_asgi_app.get("/logout")
+async def logout_get(request: Request):
+    data = dict(await request.json())
+    code = data.get('usercode')
+    game = app_manager.games[data.get('code')].game
+    app_manager.games[data.get('code')].users.players.pop(code, None)
+    app_manager.users.players.pop(code, None)
+    if (len(app_manager.games[data.get('code')].users.players) == 0):
+        app_manager.games.pop(data.get('code'), None)
+        app_manager.games.update({data.get('code'): GameData(
+            type(game)(), 0, AllPlayersData())})
+    else:
+        app_manager.games[data.get('code')] = GameData(
+            type(game)(), 0, app_manager.games[data.get('code')].users)
+    return board_data(code=data.get('code'))
+
+
+@app_manager.app.other_asgi_app.post("/logout")
+async def logout_post(request: Request):
+    data = dict(await request.json())
+    code = data.get('usercode')
+    game = app_manager.games[data.get('code')].game
+    app_manager.games[data.get('code')].users.players.pop(code, None)
+    app_manager.users.players.pop(code, None)
+    if (len(app_manager.games[data.get('code')].users.players) == 0):
+        app_manager.games.pop(data.get('code'), None)
+        app_manager.games.update({data.get('code'): GameData(
+            type(game)(), 0, AllPlayersData())})
+    else:
+        app_manager.games[data.get('code')] = GameData(
+            type(game)(), 0, app_manager.games[data.get('code')].users)
+    return board_data(code=data.get('code'))
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=5000)
+
+"""
+class AppManager():
+    def __init__(self, app):
+        self.app = app
+        self.updated_data = None
+        self.games = {}
+        self.socketIO = flask_socketio.SocketIO(
+            self.app, cors_allowed_origins='*')
+        self.users = AllPlayersData()
+app = flask.Flask(__name__)
 # Initialize the app manager
 app_manager = AppManager(app)
-
 
 @app_manager.app.route('/')
 def root():
@@ -240,3 +475,4 @@ def logout():
 if (__name__ == '__main__'):
     app_manager.socketIO.run(app_manager.app, debug=True,
                              allow_unsafe_werkzeug=True, host='0.0.0.0')
+"""
